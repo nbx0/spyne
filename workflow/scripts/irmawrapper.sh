5bfea69259d5f2d415a64fd44c75e2ac32a2c521
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2021-03-20 20:26:40 -0400

    update to use our "slither" conda env, use hidden dir for tempdir

--- workflow/scripts/irmawrapper.sh
+++ workflow/scripts/irmawrapper.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+
+# I/O handling
 irmaconfig=$1
 read1=$2
 samplename=$3
+if [[ -z "${SARS2_ONT_HOME}" ]]; then
+  SARS2_ONT_HOME='/scicomp/home-pure/sars2seq/.on-premises-ont-assembly-development/'
+fi
 
-[[ -d IRMA/_${samplename} ]] && rm -fr IRMA/_${samplename}
-spath=$(dirname $0)
-irma=$(cd $spath && readlink -f /scicomp/home-pure/sars2seq/onprem-dev/irma/IRMA)
-PATH=$(dirname $irma):${PATH}
-PERL5LIB="" &&\
-$irma $irmaconfig $read1 IRMA/_${samplename} && mv IRMA/_${samplename} IRMA/${samplename} #&& sleep 20
+# Executing this script removes any precomputed temporary data
+if [[ -d "IRMA/.${samplename}" ]]; then
+  rm -fr "IRMA/.${samplename}"
+fi
+
+# Activate "slither" conda env which contains IRMA and its dependencies
+export PATH="${SARS2_ONT_HOME}.anaconda3/bin:${PATH}"
+source activate "${SARS2_ONT_HOME}.anaconda3/env/slither"
+
+# With an empty perllib, run IRMA with a temp hidden outdir, and only if
+#  successful make the temp outdir visible to users
+PERL5LIB='' && \
+ IRMA "${irmaconfig}" "${read1}" "IRMA/.${samplename}" && \
+ mv "IRMA/.${samplename}" "IRMA/${samplename}"
