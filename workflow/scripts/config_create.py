84fe3ebe63719a01965c0359c8ef625e4c26a7ea
nbx0 <nbx0@cdc.gov>
2021-03-22 16:03:04 -0400

    test config.yaml ready

--- workflow/scripts/config_create.py
+++ workflow/scripts/config_create.py
@@ -1,26 +1,54 @@
 import yaml
 from os.path import abspath
 from sys import argv, exit
+import pandas as pd
+from glob import glob
 
 root = '/'.join(abspath(__file__).split('/')[:-3])
 
-if len(argv) != 4:
-	exit('\n\tUSAGE: {} <samplesheet.csv> <flowcell> <machine>\n'.format(__file__))
 
-data = {'machine':argv[3], 'flowcell':argv[2], 'irmamodule':'CoV-minion-long-reads', 'barcodes':{}}
+############### IN TESTING
+# FASTQ location is hardcoded here. Needs to be updated for production.
+searchDir = '/'.join(abspath(argv[1]).split('/')[:-1])
+fastqs=glob('{}/**/*.fastq.gz'.format(searchDir), recursive=True)
+###################
 
-with open('{}/resources/ont_barcodes.yaml'.format(root), 'r') as y:
-	barseqs = yaml.safe_load(y)
+
+if len(argv) < 1:
+	exit('\n\tUSAGE: {} <samplesheet.csv> <machine>\n'.format(__file__))
+
+try:
+	machine = argv[2]
+except IndexError:
+	machine = 'testMachine'
+
+data = {'machine':machine, 'irmamodule':'CoV-minion-long-reads', 'barcodes':{}}
 
 def reverse_complement(seq):
     rev = {'A':'T','T':'A','C':'G','G':'C'}
     seq = seq[::-1]
     return ''.join(rev[i] for i in seq)
 
-with open(argv[1], 'r') as d:
-	for line in d:
-		l = line.split()
-		data['barcodes'][l[0]] = {'sample':l[1], 'barcode_sequence':barseqs[l[0]], 'barcode_sequence_rc':reverse_complement(barseqs[l[0]])}
+df = pd.read_csv(argv[1])
+dfd = df.to_dict('index')
+#print(dfd)
+with open('{}/lib/{}.yaml'.format(root, dfd[0]['kit']), 'r') as y:
+	barseqs = yaml.safe_load(y)
+
+for d in dfd.values():
+	clarityid, csid, cuid, artifactid = d['alias'].split('_')
+	data['barcodes'][d['barcode']] = {'clarityid':clarityid,
+									'csid':csid,
+									'cuid':cuid,
+									'artfactid':artifactid,
+									'flow_cell_id':d['flow_cell_id'],
+									'flow_cell_product_code':d['flow_cell_product_code'],
+									'kit':d['kit'],
+									'sample_id':d['sample_id'],
+									'experiment_id':d['experiment_id'],
+									'barcode_sequence':barseqs[d['barcode']],
+									'barcode_sequence_rc':reverse_complement(barseqs[d['barcode']]),
+									'fastqs':fastqs}
 
 with open('config.yaml', 'w') as out:
 	yaml.dump(data, out, default_flow_style=False)
