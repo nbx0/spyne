03b2d16c68d2be91af10de44a179ed34f0c1932b
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2021-04-03 20:31:36 -0400

    add reporting removal counts

--- workflow/scripts/grep_summarize_cutadapt_log.sh
+++ workflow/scripts/grep_summarize_cutadapt_log.sh
@@ -10,15 +10,18 @@ cnt_input_reads=$(grep '^Total reads' "${infile}" \
 cnt_input_bases=$(grep '^Total basepairs' "${infile}" \
    | awk '{print $4}' | sed 's/,//g')
 
-# Extract adapter removal counts, calculate output read count,
-#  and extract total nucleotide output
-cnt_adapters=$(grep '^Reads with adapters:' "${infile}" \
+# Extract reads and nucleotides output
+cnt_output_reads=$(grep '^Reads written (passing filters):' "${infile}" \
+   | awk '{print $5}' | sed 's/,//g')
+cnt_output_bases=$(grep '^Total written (filtered):' "${infile}" \
    | awk '{print $4}' | sed 's/,//g')
-cnt_output_reads=$(( "${cnt_input_reads}" - "${cnt_adapters}" ))
-cnt_output_bases=$(grep '^Total written' "${infile}" \
-   | awk '{print $4}' | sed 's/,//g')
+
+# Calculate read and nucleotide removal counts
+cnt_rm_reads="$(( ${cnt_input_reads} - ${cnt_output_reads} ))"
+cnt_rm_bases="$(( ${cnt_input_bases} - ${cnt_output_bases} ))"
 
 # Form TSV information parsed from cutadapt's logfile and send to stdout
 nfo="${cnt_input_reads} input reads\t${cnt_input_bases} input bases\t"
+nfo+="${cnt_rm_reads} removed reads\t${cnt_rm_bases} removed basepairs"
 nfo+="${cnt_output_reads} output reads \t${cnt_output_bases} output bases"
 echo -e "${nfo}"
