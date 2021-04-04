e2070f4dd12651e10fc7d812ab56f1bd6b14c3d3
Chris Gulvik <chrisgulvik@users.noreply.github.com>
2021-04-03 20:32:50 -0400

    add reporting input and output counts

--- workflow/scripts/grep_summarize_reformat_log.sh
+++ workflow/scripts/grep_summarize_reformat_log.sh
@@ -10,17 +10,18 @@ cnt_input_reads=$(grep '^Input:' "${infile}" \
 cnt_input_bases=$(grep '^Input:' "${infile}" \
    | awk '{print $4}')
 
-# Extract output read and nucleotide counts, and
-#  calculate removed reads and nucleotides
+# Extract output read and nucleotide counts
 cnt_output_reads=$(grep '^Output:' "${infile}" \
    | awk '{print $2}')
 cnt_output_bases=$(grep '^Output:' "${infile}" \
    | awk '{print $5}')
+
+# Calculate removed reads and nucleotides
 cnt_discarded_reads="$(( ${cnt_input_reads} - ${cnt_output_reads} ))"
 cnt_discarded_bases="$(( ${cnt_input_bases} - ${cnt_output_bases} ))"
 
-
 # Form TSV information parsed from reformat's logfile and send to stdout
-nfo="${cnt_discarded_reads} discarded reads\t"
-nfo+="${cnt_discarded_bases} discarded bases"
+nfo="${cnt_input_reads} input reads\t${cnt_input_bases} input basepairs\t"
+nfo+="${cnt_discarded_reads} removed reads\t${cnt_discarded_bases} removed basepairs"
+nfo+="${cnt_output_reads} output reads\t${cnt_output_bases} output basepairs"
 echo -e "${nfo}"
