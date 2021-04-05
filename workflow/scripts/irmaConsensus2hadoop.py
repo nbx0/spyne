e157844b11d7374465be9a02b3b33cddecefdeca
Lacek, Kristine (CDC/DDID/NCIRD/ID) <qgx6@cdc.gov>
2021-04-05 12:43:48 -0400

    changes for hadoop upload from wkdir structure

--- workflow/scripts/irmaConsensus2hadoop.py
+++ workflow/scripts/irmaConsensus2hadoop.py
@@ -11,15 +11,15 @@ import yaml
 from datetime import datetime
 
 with open('tests/config.yaml', 'r') as y: #hard coded for testing
-	config = yaml.safe_load(y)
+        config = yaml.safe_load(y)
 
 try:
-	host=argv[1]
-	tfname=''
-	machine=config['machine']
-	runid=config['runid']
+        host=argv[1]
+        tfname=''
+        machine=config['machine']
+        runid=config['runid']
 except IndexError:
-	host=False
+        host=False
 
 path.append(dirname(dirname(realpath(__file__))))
 from DAIS_from_hadoop import log, runRibosome, wait4ribosome, loadHadoop
@@ -36,24 +36,25 @@ hadoopDB = 'sars_cov2'
 hput = dirname(dirname(realpath(__file__)))+'/scripts/hput'
 
 fastas = [i for i in glob('IRMA/*/amended_consensus/*pad.fa') if (not findall('[-_]V[0-9]',i) and getsize(i) >= 100) ]
-
+print(fastas)
 ribosome = '/'.join(realpath(__file__).split('/')[:-3])+'/dais-ribosome/ribosome'
 
 def findsampleid(filename, config):
-	for k in config['barcodes'].keys():
-		if k in filename:
-			return k
+        for k in config['barcodes'].keys():
+                if k in filename:
+                        return k
 
 tmpFile = open('hadoop/'+config['runid']+'_consensus.txt', 'w') # NamedTemporaryFile(prefix='', delete=False, mode='a+')
 tmpFileDais = open('hadoop/'+config['runid'], 'w')  #NamedTemporaryFile(prefix=dlpath, delete=False, mode='a+')
 for f in fastas:
-	sampleid = findsampleid(f, config)
-	with open(f, 'r') as d:
-		d.readline()
-		sequence = d.readline().upper().strip()
-		sha =  sha1(sequence.encode()).hexdigest()
-		tmpFile.write('\t'.join([config['machine'], config['runid'], config['barcodes'][sampleid]['csid'], config['barcodes'][sampleid]['cuid'], sequence, sha, config['barcodes'][sampleid]['clarityid'], config['barcodes'][sampleid]['Artifactid'], datetime.now().strftime('%Y-%m-%d %H:%M:%S')])+'\n')
-		tmpFileDais.write('\t'.join([sha, 'SARS-CoV-2', sequence])+'\n')
+        sampleid = findsampleid(f, config)
+        print(sampleid)
+        with open(f, 'r') as d:
+                d.readline()
+                sequence = d.readline().upper().strip()
+                sha =  sha1(sequence.encode()).hexdigest()
+                tmpFile.write('\t'.join([config['machine'], config['runid'], config['barcodes'][sampleid]['csid'], config['barcodes'][sampleid]['cuid'], sequence, sha, config['barcodes'][sampleid]['clarityid'], config['barcodes'][sampleid]['Artifactid'], datetime.now().strftime('%Y-%m-%d %H:%M:%S')])+'\n')
+                tmpFileDais.write('\t'.join([sha, 'SARS-CoV-2', sequence])+'\n')
 tmpFileDais.close()
 tmpFile.close()
 
@@ -66,5 +67,5 @@ run([hput, tmpFile.name, '/user/nbx0/sars_cov2/cdc_production_all/'])
 #print('Sending {} to loadHadoop function'.format(tmpFileDais.name))
 #loadHadoop(tmpFileDais.name)
 #for i in ['alignments', 'alignment_insertions', 'alignment_deletions', 'genomes', 'genome_insertions', 'genome_deletions']:
-#	dbc.execute('refresh {}'.format(i))
+#       dbc.execute('refresh {}'.format(i))
 
--- workflow/scripts/irmawrapper.sh
+++ workflow/scripts/irmawrapper.sh
@@ -10,8 +10,8 @@ if [[ -z "${SARS2_ONT_HOME}" ]]; then
 fi
 
 # Executing this script removes any precomputed temporary data
-if [[ -d "IRMA/.${samplename}" ]]; then
-  rm -fr "IRMA/.${samplename}"
+if [[ -d "IRMA/_${samplename}" ]]; then
+  rm -fr "IRMA/_${samplename}"
 fi
 
 spath=/scicomp/home-pure/sars2seq/prod
@@ -20,5 +20,5 @@ spath=/scicomp/home-pure/sars2seq/prod
 # With an empty perllib, run IRMA with a temp hidden outdir, and only if
 #  successful make the temp outdir visible to users
 PERL5LIB='' && \
- ${spath}/irma/IRMA "${irmaconfig}" "${read1}" "IRMA/.${samplename}" && \
- mv "IRMA/.${samplename}" "IRMA/${samplename}"
+ ${spath}/irma/IRMA "${irmaconfig}" "${read1}" "IRMA/_${samplename}" && \
+ mv "IRMA/_${samplename}" "IRMA/${samplename}"
