6a45a56ed9cdc61907836f4fadafd51c40b2cd8b
Ben Rambo-Martin <nbx0@cdc.gov>
2022-08-24 22:32:01 +0700

    ready or not

--- scripts/config_create.py
+++ scripts/config_create.py
@@ -5,49 +5,48 @@ import pandas as pd
 from glob import glob
 import subprocess
 
-root = '/'.join(abspath(__file__).split('/')[:-2])
+root = "/".join(abspath(__file__).split("/")[:-2])
 
 if len(argv) < 1:
-        exit('\n\tUSAGE: {} <samplesheet.csv> <machine_path> <runid>\n'.format(__file__))
+    exit("\n\tUSAGE: {} <samplesheet.csv> <run_id> <kit>\n".format(__file__))
 
-try:
-        machine,runid = argv[2:4]
-except (IndexError, ValueError):
-        machine,runid = 'testMachine','testRunID'
+runid, kit = argv[2:4]
 
-data = {'runid':runid, 'machine':machine, 'barcodes':{}}
+data = {"runid": runid, "barcodes": {}, "kit": kit}
+
+
 def reverse_complement(seq):
-    rev = {'A':'T','T':'A','C':'G','G':'C',',':','}
+    rev = {"A": "T", "T": "A", "C": "G", "G": "C", ",": ","}
     seq = seq[::-1]
-    return ''.join(rev[i] for i in seq)
+    return "".join(rev[i] for i in seq)
 
 
 df = pd.read_csv(argv[1])
-dfd = df.to_dict('index')
-failures = ''
+dfd = df.to_dict("index")
+failures = ""
 try:
-    with open('{}/lib/{}.yaml'.format(root, dfd[0]['Barcode_expansion_pack']), 'r') as y:
+    with open(
+        "{}/lib/{}.yaml".format(root, dfd[0]["Barcode_expansion_pack"]), "r"
+    ) as y:
         barseqs = yaml.safe_load(y)
 except:
-    with open('{}/lib/{}.yaml'.format(root, dfd[0]['kit']), 'r') as y:
+    with open("{}/lib/{}.yaml".format(root, kit, "r")) as y:
         barseqs = yaml.safe_load(y)
 for d in dfd.values():
-    fastq_pass = glob(machine + '/' + runid + '/*/*' + d['flow_cell_id'] + "*/fastq_pass/*/")
-    if d['barcode'] in [x.split("/")[-2] for x in fastq_pass]:
+    fastq_pass = glob(f"{d['barcode']}/*fastq*")
+    if d["barcode"] in [x.split("/")[-2] for x in fastq_pass]:
 
-        data['barcodes'][d['Alias']] = {
-                                                                        'flow_cell_id':d['flow_cell_id'],
-                                                                        'flow_cell_product_code':d['flow_cell_product_code'],
-                                                                        'kit':d['kit'],
-                                                                        'sample_id':d['sample_id'],
-                                                                        'experiment_id':d['experiment_id'],
-                                                                        'barcode_number':d['barcode'],
-                                                                        'barcode_sequence':barseqs[d['barcode']],
-                                                                        'barcode_sequence_rc':reverse_complement(barseqs[d['barcode']])}
+        data["barcodes"][d["sample name"]] = {
+            "kit": kit,
+            "sample_id": d["sample name"],
+            "barcode_number": d["barcode"],
+            "barcode_sequence": barseqs[d["barcode"]],
+            "barcode_sequence_rc": reverse_complement(barseqs[d["barcode"]]),
+        }
     else:
-        failures += d['barcode'] + '\n'
-with open('config.yaml', 'w') as out:
-        yaml.dump(data, out, default_flow_style=False)
+        failures += d["barcode"] + "\n"
+with open("config.yaml", "w") as out:
+    yaml.dump(data, out, default_flow_style=False)
 
 if len(failures) > 1:
-    print("failed samples detected:\n",failures.strip() )
+    print("failed samples detected:\n", failures.strip())
--- workflow/scripts/irmawrapper.sh
+++ workflow/scripts/irmawrapper.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
+fastqs=$@
+sample_name=$(echo $fastqs|cut -f1 |rev|cut -d '/' -f1|rev | cut -d '.' -f1)
+sample_name=${sample_name#cat_}
+docker run --rm \
+  -v $PWD:/data \
+  public.ecr.aws/n3z8t4o2/irma:1.0.2p3 \
+  IRMA FLU-minion $fastqs $sample_name
 
-# I/O handling
-irmaconfig=$1
-read1=$2
-samplename=$3
-if [[ -z $(which IRMA) ]]; then
-  echo "ERROR: add IRMA to path" && exit 0
-fi
-
-# Executing this script removes any precomputed temporary data
-if [[ -d "IRMA/_${samplename}" ]]; then
-  rm -fr "IRMA/_${samplename}"
-fi
-
-spath=/scicomp/home-pure/sars2seq/prod
-
-
-# With an empty perllib, run IRMA with a temp hidden outdir, and only if
-#  successful make the temp outdir visible to users
-PERL5LIB='' && \
- ${spath}/irma/IRMA "${irmaconfig}" "${read1}" "IRMA/_${samplename}" && \
- mv "IRMA/_${samplename}" "IRMA/${samplename}" && sleep 20
+[[ ! -d IRMA ]] && mkdir IRMA
+mv $sample_name IRMA/
