b471614b89bb4af12cf097f1e3739bec97a2a028
Kristine Lacek <qgx6@cdc.gov>
2022-10-25 17:46:10 -0400

    more dev, works up to permissions problem

--- workflow/scripts/daiswrapper.sh
+++ workflow/scripts/daiswrapper.sh
@@ -9,7 +9,7 @@ done
 echo $input
 readlink -f $input
 dais_out=$(echo $input|cut -d '.' -f 1) 
-[[ ! -d DAIS ]] && mkdir DAIS
+[[ ! -d IRMA/dais_results ]] && mkdir IRMA/dais_results
 cmd="docker run --rm \
     -v $PWD:/data \
     public.ecr.aws/n3z8t4o2/dais-ribosome:1.2.1 ribosome \
@@ -18,9 +18,4 @@ cmd="docker run --rm \
 echo $cmd
 eval $cmd
 
-mv ${dais_out}* DAIS/
-cd DAIS
-for i in $(cut --output-delimiter=".+" -f 3,4 ${dais_out}.seq |sort |uniq |grep -v '\\N')
-    do grep -E "${i}\s" ${dais_out}.seq | sed 's/^/>/' |cut -f 1,4,6| sed 's/\t/_/' |tr '\t' '\n' > $(echo $i |cut -d '+' -f 2)_ORF.fasta &&\
-     wslview $(echo $i |cut -d '+' -f 2)_ORF.fasta
-done
+mv ${dais_out}* IRMA/dais_results
