8773ee6d23dbacdf08da7f83d274799a035c4bcb
Kristine Lacek <qgx6@cdc.gov>
2022-10-25 14:57:42 -0400

    upgrades kiddles

--- workflow/scripts/daiswrapper.sh
+++ workflow/scripts/daiswrapper.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-input=$@
+while getopts 'i:m:' OPTION
+do
+	case $OPTION in
+	i ) input=$OPTARG;; 
+	m ) MODULE=$OPTARG;;
+	esac
+done
 echo $input
 readlink -f $input
 dais_out=$(echo $input|cut -d '.' -f 1) 
 [[ ! -d DAIS ]] && mkdir DAIS
 cmd="docker run --rm \
     -v $PWD:/data \
-    public.ecr.aws/n3z8t4o2/dais-ribosome:0.1 ribosome \
-    --module INFLUENZA $input ${dais_out}.seq ${dais_out}.ins ${dais_out}.del"
+    public.ecr.aws/n3z8t4o2/dais-ribosome:1.2.1 ribosome \
+    --module $MODULE $input ${dais_out}.seq ${dais_out}.ins ${dais_out}.del"
 
 echo $cmd
 eval $cmd
--- workflow/scripts/irmawrapper.sh
+++ workflow/scripts/irmawrapper.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 fastqs=$@
-sample_name=$(echo $fastqs|cut -f1 |rev|cut -d '/' -f1|rev | cut -d '.' -f1)
-sample_name=${sample_name#cat_}
+sample_name=$(echo $fastqs|cut -f1 |rev|cut -d '/' -f1|rev | cut -d '.' -f1 | cut -d '_' -f1)
+
 docker run --rm \
   -v $PWD:/data \
   public.ecr.aws/n3z8t4o2/irma:1.0.2p3 \
   IRMA FLU-minion $fastqs $sample_name
 
-[[ ! -d IRMA ]] && mkdir IRMA
+[[ ! -d IRMA ]] && mkdir IRMA && chmod 777 IRMA
 mv $sample_name IRMA/
